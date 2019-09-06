8e5d7fdc20198eda4fe6e8661ce796263eb55f94
KSK <kurtknosala@gmail.com>
2019-09-05 18:29:50 -0600

    mySQL connection setup

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,1 +1,38 @@
 var inquirer = require('inquirer');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "3415671Tucker.",
+    database: "bamazon_db"
+  });
+
+  connection.connect(function(err) {
+      if(err) throw err;
+      console.log('Connected as id ' + connection.threadId);
+      startUp();
+  })
+
+  function startUp() {
+      inquirer.prompt([
+          {
+              type: 'list',
+              name: 'choice',
+              message: 'What would you like to do?',
+              choices: ['Buy Something', 'Quit']
+          }
+      ]).then(function(res) {
+          switch(res.choice) {
+            case 'Buy Something':
+                console.log('Test Text');
+                startUp();
+                break;
+            case 'Quit':
+                console.log('Goodbye!');
+                connection.end();
+                break;
+          }
+      })
+  }
--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -1,1 +1,16 @@
 var inquirer = require('inquirer');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "3415671Tucker.",
+    database: "bamazon_db"
+  });
+
+  connection.connect(function(err) {
+      if(err) throw err;
+      console.log('Connected as id ' + connection.threadId);
+      connection.end();
+  })
