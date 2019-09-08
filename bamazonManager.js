e00c937694279ca13b11586400015d6f526bcc63
KSK <kurtknosala@gmail.com>
2019-09-08 12:35:01 -0600

    manager start menu complete

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,5 +1,6 @@
 var inquirer = require('inquirer');
 var mysql = require('mysql');
+var Table = require('cli-table')
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -12,5 +13,31 @@ var connection = mysql.createConnection({
   connection.connect(function(err) {
       if(err) throw err;
       console.log('Connected as id ' + connection.threadId);
-      connection.end();
+      startMenu();
   })
+
+  function startMenu() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'choice',
+            message: 'Welcome!\n What would you like to do?',
+            choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product', 'Quit']
+        }
+    ]).then(function(res) {
+        switch(res.choice) {
+            case 'View Products for Sale':
+                break;
+            case 'View Low Inventory':
+                break;
+            case 'Add to Inventory':
+                break;
+            case 'Add New Product':
+                break;
+            case 'Quit':
+                console.log('Goodbye!');
+                connection.end();
+                break;
+        }
+    })
+  }
